8d9629c711c21f5c102084d732c59ce30e831ae8
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-02 10:10:58 +0300

    Чуть переписал на ООП

--- scanner.py
+++ scanner.py
@@ -3,13 +3,29 @@ from threading import Thread
 
 N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+class Scanner:
+    def __init__(self, ip) -> None:
+        self.ip = ip
+        self.processing()
+    
+    def processing(self):
+
+        for port in range(1,N):
+            sock = socket.socket()
+            try:
+                sock.connect((self.ip, port))
+                print(f"Порт {port} открыт")
+            except:
+                print(f"Порт {port} закрыт")
+                continue
+            finally:
+                sock.close()
+
+
+def main():
+    ip_input = "127.0.0.1"
+    scanner = Scanner(ip_input)
+
+
+if __name__ == "__main__":
+    main()
