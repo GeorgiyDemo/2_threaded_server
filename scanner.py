3b0df8c80db70caf02c2c88ac7bc52ae49d8c245
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-05 21:23:28 +0300

    Получаем кол-во оставшихся заданий

--- scanner.py
+++ scanner.py
@@ -8,10 +8,12 @@ que = queue.Queue()
 results = []
 
 class Worker(threading.Thread):
+
     def __init__(self, number : int, ip : str) -> None:
         threading.Thread.__init__(self, name=f"t{number}")
         self.number = number
         self.ip = ip
+
     def run(self):
         global results
         while True:
@@ -28,11 +30,11 @@ class Worker(threading.Thread):
                     continue
                 finally:
                     sock.close()
-                
-                que.task_done()
-                #print(f"Поток {self.number} завершил выполнение задания")
+                    que.task_done()
+
+            #Елси больше нет заданий - гасим поток
             except queue.Empty:
-                print(f"Для потока {self.number} больше нет портов")
+                #print(f"Для потока {self.number} больше нет портов")
                 break
 
 class Scanner:
@@ -47,15 +49,24 @@ class Scanner:
             t.start()
         
         while True:
-            print("unfinished_tasks", que.unfinished_tasks)
-            print(results)
+            counter = que.unfinished_tasks
+            if counter == 0:
+                break
+            print(f"Осталось {counter} заданий..")
             time.sleep(5)
+
+            
+
+        self.result_output()
     
     def range_generator(self):
         """Определяет диапазоны портов для каждого потока"""
         for port in range(1,self.max_port+1):
             que.put(port)
     
+    def result_output(self):
+        print(results)
+
 def main():
     n = 2**16 - 1
     ip_input = "127.0.0.1"
