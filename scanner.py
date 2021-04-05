970b367e77596f582bd56668e68edd28ca64f9c5
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-05 17:42:17 +0300

    Worker в отдельном классе

--- scanner.py
+++ scanner.py
@@ -1,63 +1,61 @@
 import socket
-from threading import Thread
+import threading
 import queue
 import time
+
+socket.setdefaulttimeout(0.25)
+que = queue.Queue()
+results = []
+
+class Worker(threading.Thread):
+    def __init__(self, number : int, ip : str) -> None:
+        threading.Thread.__init__(self, name=f"t{number}")
+        self.number = number
+        self.ip = ip
+    def run(self):
+        global results
+        while True:
+            try:
+                current_port = que.get_nowait()
+                #print(f"Поток {self.number} взял порт {current_port}..")
+                sock = socket.socket()
+                try:
+                    sock.connect((self.ip, current_port))
+                    print(f"Порт {current_port} открыт")
+                    with threading.Lock():
+                        results.append(str(current_port))
+                except Exception as e:
+                    continue
+                finally:
+                    sock.close()
+                
+                que.task_done()
+                #print(f"Поток {self.number} завершил выполнение задания")
+            except queue.Empty:
+                print(f"Для потока {self.number} больше нет портов")
+                break
 
 class Scanner:
     def __init__(self, ip : str, max_port : int) -> None:
         
         self.ip = ip
         self.max_port = max_port
-        self.ports_for_thread = 1000
-        self.ranges_queue = queue.Queue()
+        self.result = []
         self.range_generator()
-        for i in range(100):
-            t = Thread(target=self.worker, args=(i,))
+        for i in range(300):
+            t = Worker(i, self.ip)
             t.start()
         
-        #while True:
-        #    e = self.ranges_queue.unfinished_tasks
-        #    print("Задания, которые надо выбрать:", e)
+        while True:
+            print("unfinished_tasks", que.unfinished_tasks)
+            print(results)
+            time.sleep(5)
     
     def range_generator(self):
         """Определяет диапазоны портов для каждого потока"""
-
-        ports_ranges_list = []
-        print(self.max_port)
-        for offset in range(1,self.max_port // self.ports_for_thread+1):
-            buffer = [(offset-1) * self.ports_for_thread, offset * self.ports_for_thread]
-            ports_ranges_list.append(buffer)
-        
-        #Патчим первый диапазон
-        ports_ranges_list[0][0] = 1
-        #Патчим последний диапазон
-        ports_ranges_list[-1][1] = self.max_port
-
-        #Закидываем все элементы в очередь
-        for item in ports_ranges_list:
-            self.ranges_queue.put(item)
+        for port in range(1,self.max_port+1):
+            que.put(port)
     
-    def worker(self, number : int):
-        while True:
-            try:
-                port_range = self.ranges_queue.get_nowait()
-                print(f"Поток {number} взял диапазон {port_range}..")
-                for port in range(*port_range):
-                    sock = socket.socket()
-                    try:
-                        sock.connect((self.ip, port))
-                        print(f"[Поток {number}] Порт {port} открыт")
-                    except:
-                        print(f"[Поток {number}] Порт {port} закрыт")
-                        continue
-                    finally:
-                        sock.close()
-                self.ranges_queue.task_done()
-                print(f"Поток {number} завершил выполнение задания")
-            except queue.Empty:
-                print(f"Для потока {number} больше нет диапазонов")
-                break
-
 def main():
     n = 2**16 - 1
     ip_input = "127.0.0.1"
